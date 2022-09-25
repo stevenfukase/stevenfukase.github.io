2df7403e326c626d0fdfe0c02562d397175ff805
stevenfukase <fukasesteven@gmail.com>
2022-09-25 17:53:38 +0900

    cleanup, link text fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,48 @@
 import type { NextPage } from 'next'
 import { CenteredWrapper, LinkText } from '@/parts/atoms'
 
-const Home: NextPage = () => (
+const Home: NextPage = () => {
+  const links = [
+    {
+      title: "Main site",
+      text: "stevenfukase.com",
+      href: "https://stevenfukase.com"
+    },
+    {
+      title: "GitHub",
+      text: "stevenfukase",
+      href: "https://github.com/stevenfukase"
+    },
+    {
+      title: "Instagram",
+      text: "stevenfukase",
+      href: "https://www.instagram.com/stevenfukase/"
+    },
+    {
+      title: "Random Tokyo Ward Generator",
+      text: "23wards.stevenfukase.com",
+      href: "https://23wards.stevenfukase.com"
+    }
+  ]
 
-  <CenteredWrapper>
-    <h1>SF</h1>
-    <h2>Links</h2>
-    <ul>
-      <li>
-        <h3>Main site</h3>
-        <LinkText
-          text="stevenfukase.com"
-          href="http://stevenfukase.com"
-        />
-      </li>
-      <li>
-        <h3>GitHub</h3>
-        <LinkText
-          text="stevenfukase"
-          href="https://github.com/stevenfukase"
-        />
-      </li>
-      <li>
-        <h3>Instagram</h3>
-        <LinkText
-          text="stevenfukase"
-          href="https://www.instagram.com/stevenfukase/"
-        />
-      </li>
-      <li>
-        <h3>Random Tokyo Ward Generator</h3>
-        <LinkText
-          text="ward23.stevenfukase.com"
-          href="https://23wards.stevenfukase.com"
-        />
-      </li>
-    </ul>
-  </CenteredWrapper>
-);
+
+  return (
+    <CenteredWrapper>
+      <h1>SF</h1>
+      <h2>Links</h2>
+      <ul>
+        {links.map(({ title, text, href }) => (
+          <li key={href}>
+            <h3>{title}</h3>
+            <LinkText
+              text={text}
+              href={href}
+            />
+          </li>
+        ))}
+      </ul>
+    </CenteredWrapper>
+  )
+}
 
 export default Home
